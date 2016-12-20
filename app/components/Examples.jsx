dabe6e4f21dd21acaf28d3cb5cfdf91b81853748
Snezhana Plachkova <snezhana.p.plachkova@gmail.com>
2016-12-20 17:17:02 +0200

    MASTER Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,10 @@
 var React = require('react');
 
-var Examples = (props) => <h3>Examples SF component!!!</h3> ;
+var Examples = (props) => (
+    <div>
+        <h3>Examples SF component!!!</h3>
+        <p>Welcome to Examples Page</p>
+    </div>
+    );
 
 module.exports = Examples;
