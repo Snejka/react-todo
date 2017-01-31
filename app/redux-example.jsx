2fa37ccb1540baa6fb9352613783c07cbce69d89
Snezhana Plachkova <snezhana.p.plachkova@gmail.com>
2017-01-31 18:13:35 +0200

    MASTER Adds Action-CHANGE_SEARCH

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -5,10 +5,30 @@ console.log('Starting redux Example');
 var reducer = (state = {name: 'Anonymos'}, action) => {
     //state = state || {name: 'Anonymos'};
 
-    return state;                                           //Always return the state
+    //console.log('New Action', action);
+
+    switch (action.type){
+        case 'CHANGE_NAME':
+        return {
+            ...state,
+            name: action.name
+        };
+        default:
+            return state;
+    }                                         //Always return the state
 }
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log ('Current State', currentState);
+
+
+const action = {
+    type: 'CHANGE_NAME',
+    name: 'Snezhana'
+};
+
+store.dispatch(action);
+
+console.log('Name should be changed', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -10,10 +10,27 @@ const stateDefault = {
 
 const reducer = (state = stateDefault, action) => {
 
-    return state;
+    switch (action.type){
+        case 'CHANGE_SEARCH_TEXT':
+            return {
+                ...state,
+                searchText: action.searchText
+            };
+        default:
+            return state;
+    }
 }
 
 const store = redux.createStore(reducer);
 
 const curentState = store.getState();
 console.log(curentState);
+
+const changeSerchText = {
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'New Search Text'
+}
+
+store.dispatch(changeSerchText);
+
+console.log('New State', store.getState());
