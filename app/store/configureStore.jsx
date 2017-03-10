10def4cfdfe39f23ddbb4e7cde65301fb788150e
Snezhana Plachkova <snezhana.p.plachkova@gmail.com>
2017-03-10 13:35:36 +0200

    TODOAPP-REDUX Fix for Redux Store/ Middleware

--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -2,21 +2,26 @@ import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
 import { searchTextReducer, todosReducer, showCompletedReducer } from 'reducers';
 import thunk from 'redux-thunk'; //middelware
 
-export const configure = (initialState ={}) => {
+export const configure = (initialState = {}) => {
     const reducer = combineReducers({
         searchText: searchTextReducer,
         showCompleted: showCompletedReducer,
         todos: todosReducer
     });
 
-    const store = createStore(
-        reducer,
-        initialState,
-        compose(
-            applyMiddleware(thunk),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-    );
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store = createStore(reducer, initialState, composeEnhancers(
+        applyMiddleware(thunk)
+    ));
+
+    // const store = createStore(
+    //     reducer,
+    //     initialState,
+    //     compose(
+    //         applyMiddleware(thunk),
+    //         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //     )
+    // );
 
     return store;
 }
